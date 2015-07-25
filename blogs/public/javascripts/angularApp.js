f746702c0eb304b3e6be4907b37925ffdf7e1601
frsdreams <frsdreamz@gmail.com>
2015-07-25 21:10:02 +0400

    Angular Routing & Comment
    Routing functionality  of the posts and commenting functionality of the
    post is enabled

--- blogs/public/javascripts/angularApp.js
+++ blogs/public/javascripts/angularApp.js
@@ -1,10 +1,30 @@
-var app = angular.module('blogs', []);
+var app = angular.module('blogs',  ['ui.router']);
 
 app.factory('posts', [function(){
   var o = {
     posts: []
   };
   return o;
+}]);
+
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider) {
+
+  $stateProvider
+    .state('home', {
+      url: '/home',
+      templateUrl: '/home.html',
+      controller: 'MainCtrl'
+    })
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
+    });
+
+  $urlRouterProvider.otherwise('home');
 }]);
 
 app.controller('MainCtrl', ['$scope','posts',function($scope, posts){
@@ -17,7 +37,12 @@ app.controller('MainCtrl', ['$scope','posts',function($scope, posts){
         $scope.posts.push({
                 title: $scope.title,
                 desc : $scope.desc,
-                likes: 0});
+                likes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', approvestat: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', approvestat: 0}
+                  ]
+        });
         
         $scope.title = '';
         $scope.desc = '';
@@ -25,6 +50,23 @@ app.controller('MainCtrl', ['$scope','posts',function($scope, posts){
     
     $scope.incrementLikes = function(post) {
       post.likes += 1;
+    };
+    
+}]);
+
+app.controller('PostsCtrl', ['$scope','$stateParams','posts',function($scope, $stateParams, posts){
+    
+    $scope.post = posts.posts[$stateParams.id];
+    
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        approvestat: 0
+      });
+        
+      $scope.body = '';
     };
     
 }]);
